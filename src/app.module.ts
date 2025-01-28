cd70899285d2a5ec244aaa5e188a910af3c6dfd9
Carlos Henrique Nunes <carloshnunes383@gmail.com>
2025-01-28 12:59:08 -0300

    :sparkles: Recurso CRUD - Categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriaModule } from './categoria/categoria.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
           decimalNumbers: true,
       },
     }),
+    CategoriaModule,
   ],
   controllers: [],
   providers: [],
